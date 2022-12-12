817a1180db9846461ae6828356f42e48661b72e9
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-12 11:55:49 +0530

    nestleave

---
+++ src/main/java/com/nest/nest_leave_management/NestLeaveManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nest_leave_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestLeaveManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestLeaveManagementApplication.class, args);
+	}
+
+}
