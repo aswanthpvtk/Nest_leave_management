3417cec29f777bb8696f356edc227a5664b942e2
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-12 12:34:28 +0530

    nestleave

--- src/main/java/com/nest/nest_leave_management/dao/EmployeeDao.java
+++ src/main/java/com/nest/nest_leave_management/dao/EmployeeDao.java
@@ -1,7 +1,14 @@
 package com.nest.nest_leave_management.dao;
 
 import com.nest.nest_leave_management.model.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee,Integer> {
+
+    @Query(value = "SELECT `id`, `designation`, `email`, `empid`, `empname`, `mob`, `psw`, `salary`, `uname` FROM `employee` WHERE `email`=:email AND `psw`=:psw",nativeQuery = true)
+    List<Employee> login(@Param("email") String email, @Param("psw")String psw);
 }
